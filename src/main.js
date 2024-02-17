d4fc8605233fe333928248eb9a82a7b426a5f26c
mityaiii <pericle04@mail.ru>
2024-02-17 14:58:17 +0300

    added lab

---
+++ src/main.js
@@ -0,0 +1,56 @@
+const mu = 4 * Math.PI * Math.pow(10, -7);
+
+function calculate() {
+  const radius = parseFloat(document.getElementById("input-radius").value);
+  const qty = parseFloat(document.getElementById("input-qty").value);
+  const electricity = parseFloat(document.getElementById("input-electricity").value);
+
+  if (radius <= 0) {
+    alert('radius > 0');
+    return;
+  }
+
+  if (qty <= 0) {
+    alert('qty > 0');
+    return;
+  }
+
+  if (electricity < 0) {
+    alert('electricity >= 0');
+    return;
+  }
+
+  const xValues = [];
+  const yValues = [];
+
+  const step = radius / 1000;
+  const r = radius * 50;
+
+  for (let i = -r; i <= r; i += step) {
+      xValues.push(i);
+
+      const radius2 = Math.pow(radius, 2)
+      let B = mu * qty * electricity * radius2 / 2;
+
+      const index2 = Math.pow(i, 2);
+
+      B = B * (1 / Math.pow(index2 + radius2, 1.5) + (1 / Math.pow(Math.pow(Math.abs(radius - i), 2) + radius2, 1.5)));
+
+      yValues.push(B);
+  }
+
+
+  const data = [{x: xValues, y: yValues, mode: "lines"}];
+  const layout = {
+      title: "B(x)",
+      xaxis: {
+          title: 'x, м',
+      },
+      yaxis: {
+          rangemode: 'nonnegative',
+          autorange: true,
+          title: 'В, Тл',
+      }
+  };
+  Plotly.newPlot("myPlot", data, layout);
+}
